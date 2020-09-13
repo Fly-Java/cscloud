26d84d82cfc075bcfd94c586ff87cfd0aefbf568
Java-Fly <1139160517@qq.com>
2020-09-13 15:23:10 +0800

    ''

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 #service iptables start/stop
 #防火墙启动和关闭（重启后永久性生效）
 #chkconfig iptables on/off
+#linux直接进入mysql命令
+#mysql -uroot -p
 #mysql启动/关闭
 #service mysql start/stop
 #nacos启动
 #./startup.sh -m standalone
+#查看有几个nacos启动
 #ps -ef|grep nacos|grep -v grep | wc -l
+#es启动
+#./elasticsearch -d
---
+++ cscloud-nacos_provider/src/main/java/com/zzp/nacos_provider/NacosProviderApplication.java
@@ -0,0 +1,11 @@
+package com.zzp.nacos_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+}
